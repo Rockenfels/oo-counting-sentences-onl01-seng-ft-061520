6ee208a6193aff88a234a59affbff9aa36186aa1
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-26 06:33:00 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.to_s.split(/[.!?]/)
+    return 0 if sentences.length < 1
+    sentences = sentences.reject {|i| i.empty?}
+    sentences.length
   end
 end
